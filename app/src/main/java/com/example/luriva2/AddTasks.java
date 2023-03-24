25a7e359f020b30de8afb2d4c25e18a2850e5e4d
vaiharish1 <vharish1@wpi.edu>
2023-03-24 12:16:12 -0400

    hi

--- app/src/main/java/com/example/luriva2/AddTasks.java
+++ app/src/main/java/com/example/luriva2/AddTasks.java
@@ -2,7 +2,9 @@ package com.example.luriva2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddTasks extends AppCompatActivity {
 
@@ -12,5 +14,8 @@ public class AddTasks extends AppCompatActivity {
         setContentView(R.layout.activity_add_tasks);
     }
 
-
+    public void oneTimeNav(View v){
+        Intent intent = new Intent(this, OneTimeParameters.class );
+        startActivity(intent);
+    }
 }
