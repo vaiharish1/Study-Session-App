c0828d1fecc9d5b3a4d3bedc7fe59436b3dfdc59
vaiharish1 <vharish1@wpi.edu>
2023-03-31 12:17:38 -0400

    added getters and toasts for repeated parameters screen

--- app/src/main/java/com/example/luriva2/ProjectParameters.java
+++ app/src/main/java/com/example/luriva2/ProjectParameters.java
@@ -3,6 +3,9 @@ package com.example.luriva2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ProjectParameters extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class ProjectParameters extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_parameters);
+    }
+
+    public void getTaskName(View v){
+        EditText taskName = findViewById(R.id.editTextTaskName_project);
+    }
+
+    public void collectTaskName(View v){
+        Toast toast = Toast.makeText(getApplicationContext(), "Viewing One-Time Task Manager", Toast.LENGTH_LONG);
+        toast.show();
     }
 }
--- app/src/main/java/com/example/luriva2/RepeatedParameters.java
+++ app/src/main/java/com/example/luriva2/RepeatedParameters.java
@@ -3,6 +3,9 @@ package com.example.luriva2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RepeatedParameters extends AppCompatActivity {
 
@@ -11,4 +14,69 @@ public class RepeatedParameters extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repeated_parameters);
     }
+
+    public String getTaskName() {
+        EditText taskNameText = findViewById(R.id.editTextTaskName_repeated);
+        String taskNameStr = taskNameText.getText().toString();
+        if (taskNameStr.isEmpty()) {
+            taskNameStr = "";
+        }
+        return taskNameStr;
+    }
+
+
+    public void collectTaskName(View v) {
+        String taskName = getTaskName();
+        Toast toast = Toast.makeText(getApplicationContext(), "Task Name: " + taskName, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public String getDueDate() {
+        EditText dueDateText = findViewById(R.id.editTextTaskName_repeated);
+        String dueDateStr = dueDateText.getText().toString();
+        if (dueDateStr.isEmpty()) {
+            dueDateStr = "";
+        }
+        return dueDateStr;
+    }
+
+    public void collectDueDate(View v) {
+        String dueDate = getDueDate();
+        Toast toast = Toast.makeText(getApplicationContext(), "Due Date: " + dueDate, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public String getIncrement() {
+        EditText incrementText = findViewById(R.id.editTextTaskName_repeated);
+        String incrementStr = incrementText.getText().toString();
+        if (incrementStr.isEmpty()) {
+            incrementStr = "";
+        }
+        return incrementStr;
+    }
+
+    public void collectIncrement(View v) {
+        String increment = getIncrement();
+        Toast toast = Toast.makeText(getApplicationContext(), "Increment by: " + increment + " days", Toast.LENGTH_LONG);
+        toast.show();
+
+    }
+
+    public String getTime() {
+        EditText timeText = findViewById(R.id.editTextTaskName_repeated);
+        String timeStr = timeText.getText().toString();
+        if (timeStr.isEmpty()) {
+            timeStr = "";
+        }
+        return timeStr;
+    }
+
+    public void collectTime(View v) {
+        String time = getTime();
+        Toast toast = Toast.makeText(getApplicationContext(), "Estimated time: " + time, Toast.LENGTH_LONG);
+        toast.show();
+
+    }
+
+
 }
