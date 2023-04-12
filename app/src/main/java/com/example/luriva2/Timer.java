99b3e3e902e88e5fec9e1b2639cd3255b24786ea
24lpepin <lpepin@wpi.edu>
2023-04-12 09:44:34 -0400

    mostly finished timer and fixed home page

--- app/src/main/java/com/example/luriva2/Timer.java
+++ app/src/main/java/com/example/luriva2/Timer.java
@@ -6,22 +6,33 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class Timer extends AppCompatActivity {
-    TextView status;
+    TextView status; //not done
     TextView timer;
     TextView motivationalQuote;
+
+    CountDownTimer countdown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+
         status = findViewById(R.id.timerStatus);
         timer = findViewById(R.id.timer);
         motivationalQuote = findViewById(R.id.motivationalQuote);
-        int min = 2;
-        CountDownTimer countdown = new CountDownTimer(min*60*1000,1000) {
+
+        int min = 1;
+       startTimer(min);
+    }
+
+    private void startTimer(int min){
+        countdown = new CountDownTimer(min*60*1000,1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 timer.setText(msToMin(millisUntilFinished));
@@ -29,13 +40,15 @@ public class Timer extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-                timer.setText("Done!");
+                String[] motivation = getResources().getStringArray(R.array.motivationQuotes);
+                int motivLen = motivation.length;
+                Random randy = new Random();
+                motivationalQuote.setText(motivation[randy.nextInt(motivLen)]);
+                startTimer(min);
             }
 
         };
-
-         countdown.start();
-
+        countdown.start();
     }
 
     private String msToMin(long ms) {
@@ -43,6 +56,11 @@ public class Timer extends AppCompatActivity {
         int min = (int)(ms/1000)/60;
         String time = String.format("%02d",min) + ":" + String.format("%02d",sec);
         return time;
+    }
+
+    public void cancel(View v){
+        status.setText("Timer cancelled");
+        countdown.cancel();
     }
 
 
